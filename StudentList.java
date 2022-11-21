1d9e972918181d388aec10d3e013178cf4d3459a
Shakil hossan <81898266+Shakil-RU@users.noreply.github.com>
2022-11-21 14:16:35 +0600

    Steps #0: now output matches with given output.

--- StudentList.java
+++ StudentList.java
@@ -13,7 +13,7 @@ public class StudentList {
 							new FileInputStream("students.txt"))); 
 			String r = s.readLine();
 			String i[] = r.split(",");			
-			for(String j : i) { System.out.println(j); }
+			for(String j : i) { System.out.println(j.trim()); }
 			} catch (Exception e){} 
 			System.out.println("Data Loaded.");
 		}
@@ -25,11 +25,10 @@ public class StudentList {
 					new InputStreamReader(
 							new FileInputStream("students.txt"))); 
 			String r = s.readLine();
-			System.out.println(r);
 			String i[] = r.split(",");	
 			Random x = new Random();
-				int y = x.nextInt();
-					System.out.println(i[y]);
+				int y = x.nextInt(i.length);
+					System.out.println(i[y].trim());
 			} catch (Exception e){} 
 			System.out.println("Data Loaded.");			
 		}
@@ -87,7 +86,7 @@ public class StudentList {
 					else { in_word=false;}			
 				}
 			}
-			System.out.println(count +" word(s) found " + a.length);
+			System.out.println(count +" word(s) found ");
 			} catch (Exception e){} 
 			System.out.println("Data Loaded.");				
 		}
