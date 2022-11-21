0312f7689034ebfff662873ae790414dd80149d6
Shakil hossan <81898266+Shakil-RU@users.noreply.github.com>
2022-11-21 22:14:07 +0600

    Create constants.java

---
+++ constants.java
@@ -0,0 +1,18 @@
+public class constants {
+
+    public static final String INVALID_MESSAGE = "use valid argument\n a for show all\n r for random item\n c for count\n + for add new\n ? for find specific item\n";
+    public static final String LOADED_DATA = "Loading data ...";
+    public static final String SHOW_ALL = "a";
+    public static final String COMMA = ",";
+    public static final String DATA_LOADED = "Data Loaded.";
+    public static final String RANDOM_NAME = "r";
+    public static final String PLUS = "+";
+    public static final String DATE = "dd/mm/yyyy-hh:mm:ss a";
+    public static final String LAST_UPDATE_MESSAGE = "\nList last updated on ";
+    public static final String FIND = "?";
+    public static final String WE_FOUND_MESSAGE = "We found it!";
+    public static final String COUNT = "c";
+    public static final String FOUND_MESSAGE = " word(s) found ";
+    public static final String FILE_NAME = "students.txt";
+    public static final char SPACE = ' ';
+}
